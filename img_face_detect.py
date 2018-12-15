2f2a55b62b85329a1ef189fc507ce287bcce7168
kisuns <kisuns@gmail.com>
2018-12-16 02:59:24 +0900

    20181216

---
+++ img_face_detect.py
@@ -0,0 +1,37 @@
+import os
+import cv2
+
+face_Cascade = cv2.CascadeClassifier("haarcascades\haarcascade_frontalface_alt.xml")
+eye_Cascade = cv2.CascadeClassifier("haarcascades\haarcascade_eye.xml")
+profile_Cascade = cv2.CascadeClassifier("haarcascades\haarcascade_profileface.xml")
+
+eye_detect = False
+profile_detect = False
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+img  = cv2.imread(os.getcwd() + '/files/bts2.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces   = face_Cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(60, 60))
+profile = profile_Cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(60, 60))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = img[y:y + h, x:x + w]
+    if eye_detect:
+        eyes = eye_Cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+if profile_detect:
+    for (x, y, w, h) in profile:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
